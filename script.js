f75dcf556aa7b646d5655c1988473c4b49294997
Alexej <alexej.erin2016@yandex.ru>
2019-01-22 20:55:50 +0300

    Третий Комит

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 'use strict';
 
 
-let persone = {
-   name: "John",
-   age: 25,
-   isMarried: false
-};
+//let persone = {
+//   name: "John",
+//  age: 25,
+//  isMarried: false
+//};
 
 
 
-let arr = ["plum.png","orange.jpg","apple.bmp"];
+//let arr = ["plum.png","orange.jpg","apple.bmp"];
 
-console.log(arr[0]);
+//console.log(arr[0]);
 
 // let answer = confirm("Are you here?");
 
@@ -19,20 +19,51 @@ console.log(arr[0]);
 
 //let answer = prompt("Есть ли вам 18?", "Да");
 
-let incr = 10,
-    decr = 10;
+//let incr = 10,
+//    decr = 10;
 
-console.log(incr++);
-console.log(decr--);
-console.log(5%2);
+//console.log(incr++);
+//console.log(decr--);
+//console.log(5%2);
 
-//сравнение по значениям в итоге: истина
-console.log("2" == 2);
+////сравнение по значениям в итоге: истина
+//console.log("2" == 2);
 
-//сравнение по типам в итоге: ложь
-console.log("2" === 2);
+////сравнение по типам в итоге: ложь
+//console.log("2" === 2);
 
-console.log("arr" + " - object");
+//console.log("arr" + " - object");
+
+ let num = 50;
+
+//if (num < 49) {
+   //console.log("Число меньше 49")
+//} else if (num > 100) {
+   //console.log("Много!")
+//} else {
+   //console.log("Верно!")
+//}
+
+//(num == 50) ? console.log("Верно!") : console.log(" Не верно!");
+
+switch (num) {
+   case num < 49:
+      console.log("Число меньше 49");
+      break;
+   case num > 100: 
+      console.log("Много!");
+      break;
+   case num > 80: 
+      console.log("Все еще много!");
+      break;
+   case 50: 
+      console.log("Все верно!");
+      break;
+   default:
+      console.log("Что-то пошло не так");
+      break;             
+}
+
 
 
 
