67edb2f9889bb06aed5100080451ea68d12cb7b9
Alexej <alexej.erin2016@yandex.ru>
2019-01-22 19:05:28 +0300

    The first comit

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+
+let persone = {
+   name: "John",
+   age: 25,
+   isMarried: false
+};
+
+
+
+let arr = ["plum.png","orange.jpg","apple.bmp"];
+
+console.log(arr[0]);
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+//let answer = prompt("Есть ли вам 18?", "Да");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+
+//сравнение по значениям в итоге: истина
+console.log("2" == 2);
+
+//сравнение по типам в итоге: ложь
+console.log("2" === 2);
+
+console.log("arr" + " - object");
+
+
+
+
